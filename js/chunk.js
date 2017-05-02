7713fe963c42e1eccc7957f67779c8c8ccf1f0b2
Lukas Haring García <lukasharing@hotmail.com>
2017-05-02 15:08:50 +0100

    All vars has been changed by let variables.
    preGenerate(g, l) := Values in base 10 to values in base 16.
    Tips: lines 22-23.

--- js/chunk.js
+++ js/chunk.js
@@ -7,31 +7,35 @@ class Chunk{
 
 
 	setGenerated(b)	{ this.generated = b; };
-	isGenerated()		{ return this.generated; };
+	isGenerated()	{ return this.generated; };
 
 	isInChunk(a, b){ return (a >= 0 && a < 32) && (b >= 0 && b < 32); }
 	setBlock(g, x, y, b){
 		if(this.isInChunk(x, y)){
 			this.blocks[(y << 5)|x] = b;
-			var type = b.getType(), idx = this.indexChunk;
+			let type = b.getType(), idx = this.indexChunk;
 			/* Near Block Bitwise Algorithm
-						*
-					*	b *
-						*
-				"*" means bocks next to our block "b".
+					  *
+					* b *
+					  *
+			   "*" means bocks next to our block "b".
+			   OPTIMIZE: Don't use for each neighbour the toggleNear function,
+			   save in a variable and later change it.
 			*/
-			for(var k = 1; k <= 7; k += 2){
-				var i = (k % 3), j = (k / 3)|0;
-				var xb = ((idx << 5) + x + i - 1), xc =  xb >> 5;
-				var nb = xc == idx ? this : g.getChunk(xc);
+			for(let k = 1; k <= 7; k += 2){
+				let i = (k % 3), j = (k / 3)|0;
+				let xb = ((idx << 5) + (x + i - 1)), xc = xb >> 5;
+				let nb = (xc == idx) ? this : g.getChunk(xc);
 				if(nb != undefined){
 					nb = nb.getBlock(xb, y + j - 1);
 					if(nb != undefined){
-						var t1 = nb.getType();
+						let t1 = nb.getType();
 						nb.toggleNear(1 - (t1 ^ type), 2 - i, 2 - j);
 						if(t1 == type){ b.toggleNear(1, i, j); }
 						g.mergeTextureBlock(nb);
-					}else{ b.toggleNear(0, i, j); }
+					}else{
+						b.toggleNear(0, i, j);
+					}
 				}
 				g.mergeTextureBlock(b);
 			}
@@ -39,32 +43,34 @@ class Chunk{
 	};
 
 	getBlock(x, y){
-		return this.isInChunk(x, y) ? this.blocks[(x << 5) | y] : this.blocks[0];
+		return this.isInChunk(x, y) ? this.blocks[(x << 5) | y] : null;
 	};
 
 	draw(game, ctx){
-		var camera = game.getCamera().position;
-		var hWidth = game.getWidth() >> 1, hHeight = game.getHeight() >> 1;
+		let camera = game.getCamera().position;
+		let hWidth = game.getWidth() >> 1, hHeight = game.getHeight() >> 1;
 
-		var xCamera = camera.getX();
-		var xChunk = this.indexChunk << 10;
-		var x0 = Math.max(0, Math.floor(((xCamera - hWidth - xChunk) / 32)));
-		var x1 = Math.min(32, Math.ceil(((xCamera + hWidth - xChunk) / 32)));
-
-		var yCamera = camera.getY();
-		var yChunk = (hHeight - yCamera) | 0;
-		var y0 = Math.max(0, Math.floor((yCamera - hHeight) / 32));
-		var y1 = Math.min(32, Math.ceil((yCamera + hHeight) / 32));
-
+		/* X Camera Coord Aritmetics */
+		let xCamera = camera.getX();
+		let xChunk = this.indexChunk << 10;
+		let x0 = Math.max(00, Math.floor((xCamera - hWidth - xChunk) / 32));
+		let x1 = Math.min(32, Math.ceil((xCamera + hWidth - xChunk) / 32));
+		
+		/* Y Camera Coord Aritmetics */
+		let yCamera = camera.getY();
+		let yChunk = (hHeight - yCamera) | 0;
+		let y0 = Math.max(00, Math.floor((yCamera - hHeight) / 32));
+		let y1 = Math.min(32, Math.ceil((yCamera + hHeight) / 32));
+		
 		xChunk = (xChunk + hWidth - xCamera) | 0;
 
-		for(var j = y0; j < y1; j++){
-			var jj = (j << 5);
-			var yBlock = yChunk + jj;
-			for(var i = x0; i < x1; i++){
-				var block = this.blocks[jj | i];
+		for(let j = y0; j < y1; j++){
+			let jj = (j << 5);
+			let yBlock = yChunk + jj;
+			for(let i = x0; i < x1; i++){
+				let block = this.blocks[jj | i];
 				if(block.getType() > 0){
-					var xBlock = xChunk + (i << 5);
+					let xBlock = xChunk + (i << 5);
 					block.draw(game, ctx, xBlock, yBlock);
 				}
 			}
@@ -72,11 +78,11 @@ class Chunk{
 	};
 
 	generate(g, h){
-		for(var i = 0; i < 32; i++){
-			for(var j = 0; j < h; j++){
+		for(let i = 0; i < 32; i++){
+			for(let j = 0; j < h; j++){
 				this.setBlock(g, i, j, g.block_AIR);
 			}
-			for(var j = h; j < 32; j++){
+			for(let j = h; j < 32; j++){
 				this.setBlock(g, i, j, new Ground());
 			}
 		}
@@ -84,15 +90,15 @@ class Chunk{
 	};
 
 	preGenerate(g, l){
-		for(var j = 0; j < 32; j++){
-			var jj = j * 32;
-			for(var i = 0; i < 32; i++){
-				var block;
+		for(let j = 0; j < 32; j++){
+			let jj = j * 32;
+			for(let i = 0; i < 32; i++){
+				let block = undefined;
 				switch(l[i + jj]){
-					case 0: block = new Ground(); break;
-					case 129: block = new Rock(); break;
-					case 194: block = new Spikes(); break;
-					case 225: block = new Stair(); break;
+					case 0x00: block = new Ground(); break;
+					case 0x81: block = new Rock(); break;
+					case 0xC2: block = new Spikes(); break;
+					case 0xE1: block = new Stair(); break;
 					default: block = g.block_AIR;
 				}
 				this.setBlock(g, i, j, block);
